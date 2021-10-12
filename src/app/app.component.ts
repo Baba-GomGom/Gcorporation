2e0a61136970fbf7f23a508c8c497004c2eef3e9
Bast <abdelbast.gomri@luxairgroup.lu>
2021-10-12 15:07:00 +0200

    Added particules js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { ParticlesConfig } from './particules-config';
+
+declare let particlesJS: any; // Required to be properly interpreted by TypeScript.
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Gcorporation';
+
+
+  public ngOnInit(): void {
+    this.invokeParticles();
+  }
+
+  public invokeParticles(): void {
+    particlesJS('particles-js', ParticlesConfig, function() {});
+  }
+
 }
